6f33c46b58994772a4a9a331f2f33c668752e682
Audrey Baxter <baxtea@rpi.edu>
2018-01-27 17:54:49 -0500

    Frontloaded noise generation, and raycasting the cursor works now

--- Assets/Scripts/District.cs
+++ Assets/Scripts/District.cs
@@ -10,13 +10,13 @@ public class District : MonoBehaviour {
 	// these fields are to be considered immutable as soon as Start() is called
 	//
 	[SerializeField] private SpriteRenderer rend;
+	[SerializeField] private TextMesh displayName;
 	public static uint NUM_DISTRICTS = 0; // incremented when each district is created
 
 	public string districtName;
 	public string description; // a quick description of the district
 	public Texture2D icon = null; // to display in a popup window on hover
 	Texture2D area = null; // a texture defining the location of this district through what's alpha and what's not
-	Texture2D align_tex = null; // 
 	public double sustain = 0.9; //  (0,1) exponential decay factor. number is how much enthusaism is kept through each tick
 	
 	// not sure if this field is mutable or not
@@ -25,17 +25,21 @@ public class District : MonoBehaviour {
 	//
 	// these fields can change during runtime
 	//
-
+	Texture2D align_tex = null; // 
 	Texture2D enthusaism; // monochrome texture representing how excited **the people who support you** are
 	// does a high enough enthusaism have benefits like spreading to other nearby people? TODO: test
 	// is a blob of high enough enthusaism self-sustaining by mutual excitement?
 	Texture2D distribution; // monochrome texture representing what percentage of people in a given area support you. try and black this out.
 
+
 	//
-	// used to edit the texture
+	// what the player actually sees
 	//
-	Color[] pixels; // used to edit the textures manually. pitch of area.width
 	Texture2D composite;
+
+	public void SetAlignTex(Texture2D texture) {
+		align_tex = texture;
+	}
 
 	void Awake() {
 		area = rend.sprite.texture;
@@ -45,41 +49,24 @@ public class District : MonoBehaviour {
 	// Use this for initialization
 	// something is still wonky with the scale I think
 	void Start () {
-
 		composite = new Texture2D(area.width, area.height);
-		// initialize each district texture with some noise
-		align_tex = new Texture2D(area.width, area.height);
-		enthusaism = new Texture2D(area.width, area.height);
-		distribution = new Texture2D(area.width, area.height);
-		float scale = 40.0f;
 
-		Color[] area_pixels = area.GetPixels();
-		pixels = new Color[area.width * area.height];
-
-		// enthusaism and distribution start at 0 across the board
-		for (int i = 0; i < area.height * area.width; ++i) {
-			pixels[i] = new Color(0,0,0);
-		}
-		enthusaism.SetPixels(pixels);
-		enthusaism.Apply();
-		distribution.SetPixels(pixels);
-		distribution.Apply();
-
-		// alignment (or rather, agreeability of candidate's position) uses a noise texture
+		Color[] area_pix = area.GetPixels();
+		Color[] align_pix = align_tex.GetPixels();
 		for (int r = 0; r < area.height; ++r) {
 			for (int c = 0; c < area.width; ++c) {
-				float x = c / (float)area.width * scale;
-				float y = r / (float)area.height * scale;
-				float sample = Mathf.PerlinNoise(x, y);
 
-				// restrict noise to localized area and offset by how much the player agrees
-				sample = alignment.AgreesWithPlayerFactor()*5/6 + sample/6;
-				sample *= area_pixels[r * area.width + c].a;
+				// alter the noise in the localized area to offset by how much the player agrees with local politics
+				float alpha = area_pix[r * area.width + c].a;
+				if (alpha > 0) {
+					float sample = align_pix[r * area.width + c].r; // noise as generated in Transmission.Awake
+					sample = alignment.AgreesWithPlayerFactor()*5/6 + sample/6;
 
-				pixels[r * area.width + c] = new Color(sample, sample, sample, area_pixels[r * area.width + c].a);
+					align_pix[r * area.width + c] = new Color(sample, sample, sample, alpha);
+				}
 			}
 		}
-		align_tex.SetPixels(pixels);
+		align_tex.SetPixels(align_pix);
 		align_tex.Apply();
 
 
@@ -100,7 +87,7 @@ public class District : MonoBehaviour {
 	}
 	
 	// ideally i can show like a "pulse" but i'm not very optimistic
-	void Rally (int x, int y) {
+	/*void Rally (int x, int y) {
 
 		Color[] align_pix = align_tex.GetPixels();
 		Color[] dist_pix = distribution.GetPixels();
@@ -109,14 +96,19 @@ public class District : MonoBehaviour {
 		for (int r = x-256; r < area.height && r < y+256; ++r) {
 			for (int c = y-256; c < area.width && c < y+256; ++c) {
 				// send out a wave altering distribution as it relates to alignment
+				// was planning to use inverse square falloff, but i think cosine makes more sense. cheap approximation of gaussian 
 				dist_pix[r * area.width + c] += align_pix[r * area.width + c] * NextFloat(rand);// * 1/Mathf.Pow(, 2);
 
 				// now send out a 
 			}
 		}
-	}
+	}*/
 
 	void UpdateComposite() {
 
+	}
+
+	void OnMouseOver() {
+		displayName.text = districtName;
 	}
 }
---
+++ Assets/Scripts/Transmission.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transmission : MonoBehaviour {	
+	Texture2D align_tex = null; // 
+	Texture2D enthusaism; // monochrome texture representing how excited **the people who support you** are
+	// does a high enough enthusaism have benefits like spreading to other nearby people? TODO: test
+	// is a blob of high enough enthusaism self-sustaining by mutual excitement?
+	Texture2D distribution; // monochrome texture representing what percentage of people in a given area support you. try and black this out.
+
+	// only used to reference texture. don't actually care about the contents
+	Texture2D area;
+
+	void Awake() {
+		area = gameObject.GetComponentInChildren<SpriteRenderer>().sprite.texture;
+
+		align_tex = new Texture2D(area.width, area.height);
+		enthusaism = new Texture2D(area.width, area.height);
+		distribution = new Texture2D(area.width, area.height);
+		
+		Color[] pixels = new Color[area.width * area.height];
+		for (int i = 0; i < area.height * area.width; ++i) {
+			pixels[i] = new Color(0,0,0, 0);
+		}
+		enthusaism.SetPixels(pixels);
+		enthusaism.Apply();
+		distribution.SetPixels(pixels);
+		distribution.Apply();
+
+		
+		float scale = 100.0f;
+		// alignment (or rather, agreeability of candidate's position) uses a noise texture
+		for (int r = 0; r < area.height; ++r) {
+			for (int c = 0; c < area.width; ++c) {
+				float x = c / (float)area.width * scale;
+				float y = r / (float)area.height * scale;
+				float sample = Mathf.PerlinNoise(x, y);
+
+				pixels[r * area.width + c] = new Color(sample, sample, sample, 0);
+			}
+		}
+		align_tex.SetPixels(pixels);
+		align_tex.Apply();
+
+		foreach (District d in gameObject.GetComponentsInChildren<District>()) {
+			d.SetAlignTex(align_tex);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
