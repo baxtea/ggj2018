9add219ced4876fa93f65b1211e42b8f0713c6a7
Gabriel Gray <grayg@rpi.edu>
2018-01-28 14:00:16 -0500

    added working title screen

---
+++ Assets/Scripts/Titleswitchscreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Titleswitchscreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Running");
+	}
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("loading new scene");
+            SceneManager.LoadScene("Rally Planning", LoadSceneMode.Single);
+        }
+    }
+   // void OnMouseDown()
+    //{
+       // Debug.Log("loading new scene");
+        //SceneManager.LoadScene("Rally Planning", LoadSceneMode.Single);
+    //}
+}
